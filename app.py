22a8773dc22b77bd7b98bcd8c6b8797dd12d13c9
niknguyen103-design <niknguyen103@gmail.com>
2025-12-10 11:01:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Nguyễn Văn Kín - Trường tiểu học Khuôn HÀ - ĐT: 0972 260 965</p>
 </div>
 """, unsafe_allow_html=True)
 
